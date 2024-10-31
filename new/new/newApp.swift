2065a41df7d148c2e420d67958d29ea944b1098a
Alisa Martirosyan <amartirosyan@bog.ge>
2024-10-31 18:06:14 +0400

    new project added

---
+++ new/new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Alisa Martirosyan on 31.10.24.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ new/newTests/newTests.swift
@@ -0,0 +1,16 @@
+//
+//  newTests.swift
+//  newTests
+//
+//  Created by Alisa Martirosyan on 31.10.24.
+//
+
+import Testing
+
+struct newTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
